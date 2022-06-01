b3757913b16cd7bf5f8dcd9f15c6db6461eec0ff
DylanParenzee <dylanparenzee@hotmail.com>
2022-06-01 16:41:55 +1000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code in this file!
+
+//1 ---- 
+let currentUser = "Dylan"
+
+//2----
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+
+//3----
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
+console.log(excitedWelcomeMessage)
+
+
+//4 ---
+const shortGreeting = `Welcome, ${currentUser[0]}!`
